8cab2cdcafa1bd1aa5dd7284908344973b0b7fe4
Marwane ELM <pro.marwane.elm@gmail.com>
2026-06-20 17:24:29 +0200

    some data analysis

---
+++ src/feature_selection.py
@@ -0,0 +1,7 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.inspection import permutation_importance
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
